df4c644d9c4df7f780cd448ee7a9cb9962618381
AlenPelin <AlenPelin@gmail.com>
2017-03-10 10:58:20 +1100

    Code was extracted without functional modification and hooked
    The extracted code is getting called instead of default one, but it has
    not been modified to collect extra data so far.

---
+++ src/Sitecore.Support.478956/ContentTesting/Requests/Controllers/Optimization/SupportTestsController.cs
@@ -0,0 +1,164 @@
+﻿namespace Sitecore.Support.ContentTesting.Requests.Controllers.Optimization
+{
+  using Sitecore.ContentTesting;
+  using Sitecore.ContentTesting.ContentSearch.Models;
+  using Sitecore.ContentTesting.Data;
+  using Sitecore.ContentTesting.Extensions;
+  using Sitecore.ContentTesting.Helpers;
+  using Sitecore.ContentTesting.Intelligence;
+  using Sitecore.ContentTesting.Model.Data.Items;
+  using Sitecore.ContentTesting.ViewModel;
+  using Sitecore.Data;
+  using Sitecore.Data.Items;
+  using System;
+  using System.Collections.Generic;
+  using System.Linq;
+  using System.Web.Http;
+  using System.Web.Http.Results;
+
+  public class SupportTestsController : Sitecore.ContentTesting.Requests.Controllers.Optimization.TestsController
+  {
+    // copy of base value
+    private readonly IContentTestStore _contentTestStore;
+
+    public SupportTestsController() 
+    {
+      // copied from base class implementation
+      _contentTestStore = ContentTestingFactory.Instance.ContentTestStore;
+    }
+
+    public SupportTestsController(IContentTestStore contentTestStore) : base(contentTestStore)
+    {
+      _contentTestStore = contentTestStore;
+    }
+
+    [HttpGet]
+    public JsonResult<TestListViewModel> GetActiveTestsEx(int? page = new int?(), int? pageSize = new int?(), string hostItemId = null, string searchText = null)
+    {
+      page = new int?(page.HasValue ? page.GetValueOrDefault() : 1);
+      pageSize = new int?(pageSize.HasValue ? pageSize.GetValueOrDefault() : 20);
+
+      DataUri hostItemDataUri = null;
+      if (!string.IsNullOrEmpty(hostItemId))
+      {
+        hostItemDataUri = DataUriParser.Parse(hostItemId, "");
+      }
+
+      var activeTests = ContentTestStore.GetActiveTests(hostItemDataUri, searchText, null)
+        .ToArray();
+
+      var items = new List<ExecutedTestViewModel>();
+      var dictionary = new Dictionary<ID, ITestConfiguration>();
+
+      foreach (var activeTest in activeTests)
+      {
+        Item item2 = Database.GetItem(activeTest.Uri);
+        if (item2 != null)
+        {
+          var testDefinitionItem = TestDefinitionItem.Create(item2);
+          if (testDefinitionItem != null)
+          {
+            var hostItem = (activeTest.HostItemUri != null) ? item2.Database.GetItem(activeTest.HostItemUri) : null;
+            if (hostItem != null)
+            {
+              var testConfiguration = _contentTestStore.LoadTestForItem(hostItem, testDefinitionItem);
+              if (testConfiguration != null)
+              {
+                var testId = testConfiguration.TestDefinitionItem.ID;
+                dictionary.Add(testId, testConfiguration);
+
+                var model = new ExecutedTestViewModel
+                {
+                  HostPageId = hostItem.ID.ToString(),
+                  HostPageUri = hostItem.Uri.ToDataUri(),
+                  HostPageName = hostItem.DisplayName,
+                  DeviceId = testConfiguration.DeviceId.ToString(),
+                  DeviceName = testConfiguration.DeviceName,
+                  Language = testConfiguration.LanguageName,
+                  CreatedBy = FormattingHelper.GetFriendlyUserName(item2.Security.GetOwner()),
+                  Date = DateUtil.ToServerTime(testDefinitionItem.StartDate).ToString("dd-MMM-yyyy"),
+                  ExperienceCount = testConfiguration.TestSet.GetExperienceCount(),
+                  Days = GetEstimatedDurationDays(hostItem, testConfiguration.TestSet.GetExperienceCount(), testDefinitionItem),
+                  ItemId = testDefinitionItem.ID.ToString(),
+                  ContentOnly = testConfiguration.TestSet.Variables.Count == testDefinitionItem.PageLevelTestVariables.Count,
+                  TestType = testConfiguration.TestType,
+                  TestId = testId
+                };
+
+                items.Add(model);
+              }
+            }
+          }
+        }
+      }
+      
+      items = items
+        .OrderBy(x => x.Days)
+        .Skip(((page.Value - 1) * pageSize.Value))
+        .Take(pageSize.Value).ToList();
+
+      foreach (ExecutedTestViewModel executedTest in items)
+      {
+        if (Database.GetItem(executedTest.HostPageUri) != null)
+        {
+          executedTest.Effect = GetWinningEffect(dictionary[executedTest.TestId]);
+          if (executedTest.Effect < 0.0)
+          {
+            executedTest.EffectCss = "value-decrease";
+          }
+          else if (executedTest.Effect == 0.0)
+          {
+            executedTest.EffectCss = "value-nochange";
+          }
+          else
+          {
+            executedTest.EffectCss = "value-increase";
+          }
+        }
+      }
+
+      var content = new TestListViewModel
+      {
+        Items = items,
+        TotalResults = activeTests.Count()
+      };
+
+      return Json(content);
+    }
+
+    private double GetWinningEffect(ITestConfiguration test)
+    {
+      var performanceForTest = PerformanceFactory.GetPerformanceForTest(test);
+      if (performanceForTest.BestExperiencePerformance != null)
+      {
+        return performanceForTest.GetExperienceEffect(performanceForTest.BestExperiencePerformance.Combination, false);
+      }
+
+      return 0.0;
+    }
+
+    private int GetEstimatedDurationDays(Item hostItem, int experienceCount, TestDefinitionItem testDef)
+    {
+      string deviceName = string.Empty;
+      if (testDef.Device.TargetItem != null)
+      {
+        deviceName = testDef.Device.TargetItem.Name;
+      }
+
+      var testRunEstimator = ContentTestingFactory.Instance.GetTestRunEstimator(testDef.Language, deviceName);
+      testRunEstimator.HostItem = hostItem;
+
+      var estimate = testRunEstimator.GetEstimate(experienceCount, 0.8, testDef.TrafficAllocationPercentage, testDef.ConfidenceLevelPercentage, testDef, TestMeasurement.Undefined);
+
+      var timeSpan = testDef.StartDate.AddDays(estimate.EstimatedDayCount.HasValue ? estimate.EstimatedDayCount.Value : 0.0) - DateTime.UtcNow;
+      int totalDays = (int)Math.Ceiling(timeSpan.TotalDays);
+      if (totalDays < 1)
+      {
+        return int.Parse(testDef.MaxDuration);
+      }
+
+      var difference = DateTime.UtcNow - testDef.StartDate;
+      return Math.Max(Math.Min(totalDays, int.Parse(testDef.MaxDuration) - difference.Days), int.Parse(testDef.MinDuration) - difference.Days);
+    }
+  }
+}
---
+++ src/Sitecore.Support.478956/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.478956")]
+[assembly: AssemblyProduct("Sitecore.Support.478956")]
+[assembly: ComVisible(false)]
